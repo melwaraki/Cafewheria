ff177f41035ca1c8431593a164b3432b22be4244
Marwan Elwaraki <m.elwaraki@icloud.com>
2021-09-27 00:05:16 +0100

    add non-functioning i'm feeling lucky button

--- Cafewheria/Controller/Venues/VenuesViewController.swift
+++ Cafewheria/Controller/Venues/VenuesViewController.swift
@@ -14,6 +14,7 @@ class VenuesViewController: UIViewController {
     @IBOutlet weak var emptyScreenLabel: UILabel!
     @IBOutlet weak var toggleLocationButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var feelingLuckyButton: UIButton!
     
     var locationManager: CLLocationManager!
     var toggleableViews: [UIView] = []
@@ -22,8 +23,9 @@ class VenuesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        toggleableViews = [activityIndicatorView, emptyScreenLabel, toggleLocationButton, tableView, feelingLuckyButton]
         setupLocationManager()
-        toggleableViews = [activityIndicatorView, emptyScreenLabel, toggleLocationButton, tableView]
+        decorateInterface()
     }
     
     @IBAction func tappedToggleLocationButton(_ sender: Any) {
@@ -34,6 +36,14 @@ class VenuesViewController: UIViewController {
         UIApplication.shared.open(url)
     }
     
+    @IBAction func tappedFeelingLucky(_ sender: Any) {
+        presentErrorAlert(message: "You're in early")
+    }
+    
+    func decorateInterface() {
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 100))
+        feelingLuckyButton.addShadow()
+    }
     
     var state: ViewControllerState = .loading {
         didSet {
@@ -60,7 +70,7 @@ class VenuesViewController: UIViewController {
         case .success:
             activityIndicatorView.stopAnimating()
             tableView.reloadData()
-            hideAll(except: [tableView])
+            hideAll(except: [tableView, feelingLuckyButton])
         }
     }
     
@@ -71,4 +81,15 @@ class VenuesViewController: UIViewController {
     }
     
     
+}
+
+extension UIView {
+    func addShadow(color: UIColor = .label) {
+        clipsToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 3)
+        layer.shadowRadius = 6.0
+        layer.shadowOpacity = 0.8
+        layer.masksToBounds = false
+    }
 }
