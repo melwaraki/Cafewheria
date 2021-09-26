9a84ad9bb9103cecd7b3d336888030f04f88f0cd
Marwan Elwaraki <m.elwaraki@icloud.com>
2021-09-26 20:00:24 +0100

    add URLTests class

---
+++ CafewheriaTests/URLTests.swift
@@ -0,0 +1,25 @@
+//
+//  URLTests.swift
+//  CafewheriaTests
+//
+//  Created by Marwan Elwaraki on 26/09/2021.
+//
+
+import XCTest
+@testable import Cafewheria
+
+class URLTests: XCTestCase {
+
+    func testBuildURL() {
+        
+        let url = Router().buildURL(path: "venues/search", queryItems: [
+            URLQueryItem(name: "ll", value: "\(12.34),\(56.78)"),
+            URLQueryItem(name: "categoryId", value: "1234"),
+        ])
+        
+        let expectedURL = URL(string: "https://api.foursquare.com/v2/venues/search?ll=12.34,56.78&categoryId=1234&client_secret=\(Router().clientSecret)&client_id=\(Router().clientID)&v=\(Router().versionNumber)")
+        
+        XCTAssertEqual(url, expectedURL)
+    }
+
+}
